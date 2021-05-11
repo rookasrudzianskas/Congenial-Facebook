8508acfd21a968041b23c37a2f1c6f057476b614
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 10:34:13 +0300

    Roakas

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./styles/Feed.css";
+import StoryReel from "./StoryReel";
 
 const Feed = () => {
     return (
         <div className="feed">
+            <StoryReel />
         {/*    story        */}
         {/*    Message Sender   */}
 
---
+++ src/components/StoryReel.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./styles/StoryReel.css";
+import Story from "./Story";
+
+const StoryReel = () => {
+    return (
+        <div className="storyReel">
+
+            <Story
+                image="https://i1.wp.com/www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png?fit=750%2C425&quality=50&ssl=1"
+                profileSrc="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg"
+                title="Rookas Rudzianskas"
+            />
+
+            <Story
+                image="https://i1.wp.com/www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png?fit=750%2C425&quality=50&ssl=1"
+                profileSrc="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg"
+                title="Rookas Rudzianskas"
+            />
+
+            <Story
+                image="https://i1.wp.com/www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png?fit=750%2C425&quality=50&ssl=1"
+                profileSrc="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg"
+                title="Rookas Rudzianskas"
+            />
+
+            <Story
+                image="https://i1.wp.com/www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png?fit=750%2C425&quality=50&ssl=1"
+                profileSrc="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg"
+                title="Rookas Rudzianskas"
+            />
+
+
+        </div>
+    );
+};
+
+export default StoryReel;
