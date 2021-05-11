48a40d71f597c544212d3b20c10ab5e2d93ec4a5
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 10:26:44 +0300

    Roakas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className="app__body">
             <Sidebar />
             {/*    Sidebar      */}
+            <Feed />
             {/*    Feed     */}
             {/*        Widgets*/}
         </div>
---
+++ src/components/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles/Feed.css";
+
+const Feed = () => {
+    return (
+        <div className="feed">
+        {/*    story        */}
+        {/*    Message Sender   */}
+
+        </div>
+    );
+};
+
+export default Feed;
