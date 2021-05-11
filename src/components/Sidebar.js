69fe49341dbc4595d112d57f4bd1643ffec20468
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 10:06:44 +0300

    Roakas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import "./styles/Sidebar.css";
 import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <SidebarRow />
-            <SidebarRow />
-            <SidebarRow />
-            <SidebarRow />
-            <SidebarRow />
+            <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+            <SidebarRow Icon={PeopleIcon} title="Friends" />
+            <SidebarRow Icon={ChatIcon} title="Messenger" />
+            <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+            <SidebarRow Icon={ExpandMoreIcon} title="Marketplace" />
 
         </div>
     );
