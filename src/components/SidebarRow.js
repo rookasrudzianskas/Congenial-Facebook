0e011029efef754090be17df246ed79a0eb5f91b
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 10:12:28 +0300

    Roakas

--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import "./styles/SidebarRow.css";
+import {Avatar} from "@material-ui/core";
 
 const SidebarRow = ({src, Icon, title}) => {
     return (
         <div className="sidebarRow">
+             {/*if we have src props, so we render the icon too*/}
+            {src && <Avatar src={src} />}
+            {/* if we have an icon prop, so we render the icon too!*/}
+            {Icon && <Icon />}
             <p>{title}</p>
         </div>
     );
-};
+}
 
 export default SidebarRow;
