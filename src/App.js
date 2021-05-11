8c7468440f230efa34a75d3dae784917bf7e6389
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 18:13:10 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
             {/*    Sidebar      */}
             <Feed />
             {/*    Feed     */}
+            <Widgets />
             {/*        Widgets*/}
         </div>
 
---
+++ src/components/Widgets.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/Widgets.css";
+
+const Widgets = () => {
+    return (
+        <div className="widgets">
+
+        </div>
+    );
+};
+
+export default Widgets;
