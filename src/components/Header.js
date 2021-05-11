0e8bad12a766c1e4f16562dc5f929cdd41000469
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 09:17:16 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
     return (
         <div className="header">
             <div className="header__left">
-
+                <img
+                    src="https://iconape.com/wp-content/files/yd/117914/svg/Facebook_f_logo__2019_.svg"
+                    alt=""
+                />
             </div>
 
             <div className="header__middle">
