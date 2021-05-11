63b063af6d49185c641f5f729f4821ca8abd2abc
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 10:35:16 +0300

    Roakas

--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -30,6 +30,13 @@ const StoryReel = () => {
                 title="Rookas Rudzianskas"
             />
 
+            <Story
+                image="https://i1.wp.com/www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png?fit=750%2C425&quality=50&ssl=1"
+                profileSrc="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg"
+                title="Rookas Rudzianskas"
+            />
+
+
 
         </div>
     );
