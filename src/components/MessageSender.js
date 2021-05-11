c62931b1f544d741a198f88c208da381f78d2325
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 17:07:31 +0300

    Rokas

--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./styles/MessageSender.css";
 import {Avatar} from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 const MessageSender = () => {
 
@@ -21,6 +24,21 @@ const MessageSender = () => {
             </div>
 
             <div className="messageSender__bottom">
+
+                    <div className="messageSender__option">
+                        <VideocamIcon style={{ color: "red" }} />
+                        <h3>Live Video</h3>
+                    </div>
+
+                    <div className="messageSender__option">
+                        <PhotoLibraryIcon style={{ color: "green" }} />
+                        <h3>Photo/Video</h3>
+                    </div>
+
+                    <div className="messageSender__option">
+                        <InsertEmoticonIcon style={{ color: "orange" }} />
+                        <h3>Feeling/Activity</h3>
+                    </div>
 
             </div>
         </div>
