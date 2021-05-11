d7610fe6f1a766bc3e4903515368c2e9e686908a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 16:58:04 +0300

    Rokas

--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./styles/MessageSender.css";
+import {Avatar} from "@material-ui/core";
 
 const MessageSender = () => {
     return (
         <div className="messageSender">
-            
+            <div className="messageSender__top">
+                <Avatar />
+            </div>
+
+            <div className="messageSender__bottom">
+
+            </div>
         </div>
     );
 };
