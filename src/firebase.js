0b2b90941e0524555f37a9860e0f4971aacd1fe7
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 05:49:47 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAIIrnugSJVuRPSCmPUrDbMACiSVDsV-f4",
@@ -8,3 +10,13 @@ const firebaseConfig = {
     appId: "1:935126950289:web:b6876e8e5cb3b635f4ba30",
     measurementId: "G-FS4K28SQ2S"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+// setting the providers, that we want the Google or etc login servers
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+// to use normally
+export default db;
