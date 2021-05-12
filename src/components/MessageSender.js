2b49f5dde796cf5aec5747224a38945b5f3b008d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 06:35:53 +0300

    Rokas

--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from "@material-ui/icons/Videocam";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import {useStateValue} from "../StateProvider";
+import db from "../firebase";
+import firebase from "firebase";
 
 const MessageSender = () => {
 
@@ -15,7 +17,13 @@ const MessageSender = () => {
     const handleSubmit = e => {
         e.preventDefault();
     //   some clever db stuff
-
+        db.collection("posts").add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            profilePic: user.photoURL,
+            username: user.displayName,
+            image: imageUrl,
+        })
         setInput("");
         setImageUrl("");
     };
